721a181986a8bcf518fc45f0eab78af45141bf76
yuizho <yuizho3@gmail.com>
2020-05-12 23:17:17 +0900

    modify sw.js to only fetch images

--- static/sw.js
+++ static/sw.js
@@ -3,11 +3,13 @@ const cacheName = 'v1-yuizho-blog';
 // Call Install Event
 self.addEventListener('install', event => {
     console.log("Service Worker: Installed");
+    event.waitUntil(self.skipWaiting());
 });
 
 //  Call Activate Event
 self.addEventListener('activate', event => {
     console.log("Service Worker: Activated");
+    event.waitUntil(self.clients.claim());
 
     //  Remove unwanted  caches (the caches in the older version Cache Stroage)
     event.waitUntil(
@@ -26,29 +28,23 @@ self.addEventListener('activate', event => {
 
 // Call Fetch Event
 self.addEventListener('fetch', event => {
-    console.log('Service Worker: Fetching');
+    // if  the request is image, return immediately.
+    if (!/(\.webp|\.jpeg|\.jpg|\.gif|\.png)$/.test(event.request.url)) {
+        return;
+    }
+
     event.respondWith(async function () {
         // if the request exists in cache, return it.
         const cachedResponse = await caches.match(event.request);
         if (cachedResponse) {
+            console.log(`get from cache: ${event.request.url}`);
             return cachedResponse
         }
 
-        return fetch(event.request)
-            .then(res => {
-                // Make copy /clone of response
-                const resClone = res.clone();
-                // Open cache
-                caches
-                    .open(cacheName)
-                    .then(cache => {
-                        // add response to cache
-                        cache.put(event.request, resClone);
-                    });
-                return res;
-            }).catch(err => {
-                // if there is no connection, the cache is used
-                caches.match(event.request).then(res => res);
-            });
+        // if the request inexists in cache, fetch the asset and  store put into cache.
+        const fetchedResponse = await fetch(event.request);
+        const opennedCache = await caches.open(cacheName);
+        opennedCache.put(event.request, fetchedResponse.clone());
+        return fetchedResponse;
     }());
 });
